016644d60e1305e86945d3503716b6981a7971ec
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2021-02-25 20:41:00 +0100

    getProduct/updateProduct/deleteProduct

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -2,10 +2,17 @@ const Product = require('../models/product')
 
 
 
-exports.getProducts=(req,res,next)=>{
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find()
+    const productsCount = await Product.countDocuments();
+
     res.status(200).json({
-        success:true,
-        message:"All products"
+        success: true,
+        productsCount,
+        products,
+
+
     })
 }
 
@@ -16,6 +23,63 @@ exports.newProduct = async (req, res, next) => {
 
     res.status(201).json({
         success: true,
+        product
+    })
+}
+
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id)
+    console.log(product)
+
+    if (!product) {
+        res.status(404).json({
+            success: false,
+            message: "Nie znaleziono produktu"
+        })
+    }
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+
+
+// Update Product   =>   /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        res.status(404).json({
+            success: false,
+            message: "Nie znaleziono produktu"
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+// Delete Product   =>   /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+        res.status(404).json({
+            success: false,
+            message: "Nie znaleziono produktu"
+        })
+    }
+    product = await product.remove()
+    res.status(200).json({
+        message: "Pomyślnie usunięto poniższy produkt",
         product
     })
 }
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,13 @@
 const express=require('express')
 const router =express.Router()
 
-const { getProducts, newProduct } = require('../controllers/productControllers')
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productControllers')
 
 router.get('/products', getProducts)
 router.post('/admin/product/new', newProduct)
+router.get('/product/:id', getSingleProduct)
+router.put('/admin/product/:id',  updateProduct)
+router.delete('/admin/product/:id', deleteProduct)
+
 
 module.exports = router
