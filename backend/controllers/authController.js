a12d23c20f118b555ec71ae2dbe55ad488658cb7
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2021-02-26 12:02:42 +0100

    logout user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -53,10 +53,20 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     }
 
     const token = user.getJwtToken()
-
-
     sendToken(user, 200, res)
-
-
 })
+
+
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+    res.cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: 'Wylogowano użytkownika'
+    })
+})
+
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express=require('express')
 const router =express.Router()
 
-const {registerUser, loginUser} = require('../controllers/authController')
+const {registerUser, loginUser, logoutUser} = require('../controllers/authController')
 
 const { isAuthenticatedUser } = require('../middlewares/auth')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
+router.get('/logout', logoutUser)
 
 
 
