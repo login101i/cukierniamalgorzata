aa99041f4e07fa3f4fc7a9fdb838f42953a08287
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2021-02-26 12:49:30 +0100

    generateResetPasswordToken method in user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,6 +3,9 @@ const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 
+const crypto = require('crypto')
+
+
 
 
 
@@ -70,6 +73,21 @@ userSchema.methods.getJwtToken = function () {
 // Compare user password
 userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
+}
+
+// Generate password reset token
+userSchema.methods.getResetPasswordToken = function () {
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash and set to resetPasswordToken
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+
+    // Set token expire time
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000
+
+    return resetToken
+
 }
 
 
