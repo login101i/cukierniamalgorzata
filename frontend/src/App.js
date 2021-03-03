f8a044dfebf93c18f0bd10895dd5e9a61b40861d
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2021-03-03 11:41:17 +0100

    cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import UpdateProfile from './components/user/UpdateProfile'
 import UpdatePassword from './components/user/UpdatePassword'
 import ForgotPassword from './components/user/ForgotPassword'
 import NewPassword from './components/user/NewPassword'
+import Cart from './components/cart/Cart'
 
 
 
@@ -37,7 +38,6 @@ function App() {
         <Route path="/" component={Home} exact />
         <Route path="/search/:keyword" component={Home} exact />
         <Route path="/product/:id" component={ProductDetails} exact />
-        <Footer />
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
         <ProtectedRoute path="/me" component={Profile} exact />
@@ -45,9 +45,11 @@ function App() {
         <ProtectedRoute path="/password/update" component={UpdatePassword} exact />
         <Route path="/password/forgot" component={ForgotPassword} exact />
         <Route path="/password/reset/:token" component={NewPassword} exact />
-
-
+        <Route path="/cart" component={Cart} exact />
       </div>
+      
+    <Footer />
+
     </Router>
   );
 }
---
+++ frontend/src/components/cart/Cart.js
@@ -0,0 +1,86 @@
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+
+import MetaData from '../layout/MetaData'
+
+import { useAlert } from 'react-alert'
+import { useDispatch, useSelector } from 'react-redux'
+import { addItemToCart } from '../../actions/cartActions'
+
+const Cart = ({ history }) => {
+
+    const dispatch = useDispatch();
+
+    const { cartItems } = useSelector(state => state.cart)
+
+
+    return (
+        <Fragment>
+            <MetaData title={'Twój koszyk'} />
+            {cartItems.length === 0 ? <h2 className="mt-5">Twój koszyk jest pusty.</h2> : (
+                <Fragment>
+                    <h2 className="mt-5">Twój koszyk: <b>{cartItems.length} pozycji.</b></h2>
+
+                    <div className="row d-flex justify-content-between">
+                        <div className="col-12 col-lg-8">
+
+                            {cartItems.map(item => (
+                                <Fragment>
+                                    <hr />
+
+                                    <div className="cart-item" key={item.product}>
+                                        <div className="row">
+                                            <div className="col-4 col-lg-3">
+                                                <img src={item.image} alt="Tort" height="90" width="115" />
+                                            </div>
+
+                                            <div className="col-5 col-lg-3">
+                                                <Link to={`/products/${item.product}`}>{item.name}</Link>
+                                            </div>
+
+
+                                            <div className="col-4 col-lg-2 mt-4 mt-lg-0">
+                                                <p id="card_item_price">{item.price} zł</p>
+                                            </div>
+
+                                            <div className="col-4 col-lg-3 mt-4 mt-lg-0">
+                                                <div className="stockCounter d-inline">
+                                                    <span className="btn btn-danger minus">-</span>
+
+                                                    <input type="number" className="form-control count d-inline" value={item.quantity} readOnly />
+
+                                                    <span className="btn btn-primary plus">+</span>
+                                                </div>
+                                            </div>
+
+                                            <div className="col-4 col-lg-1 mt-4 mt-lg-0">
+                                                <i id="delete_cart_item" className="fa fa-trash btn btn-danger"  ></i>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <hr />
+                                </Fragment>
+                            ))}
+
+                        </div>
+
+                        <div className="col-12 col-lg-3 my-4">
+                            <div id="order_summary">
+                                <h4>Podsumowanie</h4>
+                                <hr />
+                                <p>Razem:  <span className="order-summary-values">{cartItems.reduce((acc, item) => (acc + Number(item.quantity)), 0)} (Pozycji)</span></p>
+                                <p>Razem razem: <span className="order-summary-values">{cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0).toFixed(2)} zł</span></p>
+
+                                <hr />
+                                <button id="checkout_btn" className="btn btn-primary btn-block">Do kasy</button>
+                            </div>
+                        </div>
+                    </div>
+                </Fragment>
+            )}
+        </Fragment>
+    )
+}
+
+export default Cart
